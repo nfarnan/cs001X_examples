63a3c6406cde97d5892c243eeab317a209833f62
Nick Farnan <nlf4@pitt.edu>
2020-03-04 13:45:35 -0500

    Adding code from lecture on 3/4

--- dictionaries/MW/03_process_grades.py
+++ dictionaries/MW/03_process_grades.py
@@ -1,35 +1,51 @@
-NAME = 0
-MIDTERM = 1
-FINAL = 2
-PROJECTS = 3
-
 def read_file(fname):
 	main_dict = {}
 	with open(fname) as infile:
+		headers = infile.readline()
+		headers = headers.strip().split(",")
+		# testing file parsing
+		#print("headers:")
+		#print(headers)
+		#print("rest of the file:")
+
 		for line in infile:
 			clean_line = line.strip()
 			cur_row = clean_line.split(",")
-			nested_dict = {"Midterm":cur_row[MIDTERM], \
-				"Final":cur_row[FINAL], \
-				"Projects":cur_row[PROJECTS]}
-			main_dict[cur_row[NAME]] = nested_dict
+			# testing row parsing
+			#print(cur_row)
+
+			nested_dict = {}
+			for i in range(1, len(cur_row)):
+				nested_dict[headers[i]] = cur_row[i]
+
+			# testing creation of nested_dict
+			#print(nested_dict)
+
+			main_dict[cur_row[0]] = nested_dict
 
 	return main_dict
 
 def print_midterms(grades):
-	for sublist in grades:
-		print(sublist[NAME], ": ", sublist[MIDTERM], sep="")
+	print("Name, Midterm score")
+	for key in grades:
+		print(key, ": ", grades[key]["Midterm"])
+
+	print()
 
 def print_for_student(grades, stud_name):
-	for row in grades:
-		if row[NAME] == stud_name:
-			print(stud_name, "got:")
-			print("\tMidterm:", row[MIDTERM])
-			print("\tFinal:", row[FINAL])
-			print("\tProjects:", row[PROJECTS])
+	print(stud_name, "'s grades:", sep="")
+	for key in grades[stud_name]:
+		print("\t", key, ": ", grades[stud_name][key])
+
+	print()
 
 def main():
 	grades_nd = read_file("fake_grades.csv")
-	print(grades_nd)
+	
+	# debugging result of read_file
+	#print(grades_nd)
+
+	print_midterms(grades_nd)
+	print_for_student(grades_nd, "Bob")
 
 main()
---
+++ dictionaries/MW/04_dict_loop.py
@@ -0,0 +1,6 @@
+d = {"one":1, "two":2, "three":3}
+
+for k in d:
+	print(k)
+	print(d[k])
+	print()
