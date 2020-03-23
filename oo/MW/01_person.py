f0aa1fcb82d7551d82f2821d490a24e9f2a43b72
Nick Farnan <nlf4@pitt.edu>
2020-03-23 14:45:04 -0400

    Adding code from lecture on 3/23

---
+++ oo/MW/01_person.py
@@ -0,0 +1,36 @@
+class Person:
+	def __init__(self, l_name, l_age=20):
+		self.names = [l_name]
+		self.age = l_age
+	
+	def display(self):
+		print("Names:", self.names)
+		print("Age:", self.age)
+
+	def setAge(self, new_age):
+		self.age = new_age
+
+	def addAlias(self, alias):
+		self.names.append(alias)
+
+	def __str__(self):
+		return self.names[0]
+
+	def __repr__(self):
+		rv = "Person("
+		rv += str(self.names)
+		rv += ", "
+		rv += str(self.age)
+		rv += ")"
+
+		return rv
+
+a = Person("Alice", 30)
+a_string = str(a)
+print(a.__str__())
+print(repr(a))
+a_representation = a.__repr__()
+print(a)
+print(a_string)
+print(a_representation)
+
